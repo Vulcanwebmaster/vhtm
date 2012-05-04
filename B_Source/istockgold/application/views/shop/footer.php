e54fb521d3dd71c0f07c6b48bdc1f2efd199fac1
tungns2408 <tungns2408@gmail.com>
2012-05-04 11:20:16 +0000

    Update footer

--- B_Source/istockgold/application/views/shop/footer.php
+++ B_Source/istockgold/application/views/shop/footer.php
@@ -1,18 +1,18 @@
 <div id="footer">
 		<div>
-			<a>Home</a>
+			<a href = "<?php  echo base_url()?>" >Home</a>
 			|
 			<a>Reserves Notification</a>
 			|
 			<a>Exchange e-Currency</a>			
 			|
-			<a>Faqs</a>
+			<a href = "<?php  echo base_url()?>index.php/welcome/faq">Faqs</a>
 			|
-			<a>Track Order</a>
+			<a href = "<?php  echo site_url($module."/order");?>" >Track Order</a>
 			|
 			<a>News</a>
 			|
-			<a>Contact Us</a>
+			<a href = "<?php  echo base_url()?>index.php/welcome/contact" >Contact Us</a>
 		</div> 
 		<p>Copyright &copy 2011 Yahoo! Inc. All Rights Reserved.</p>
 	</div><!-- END of FOOTER  -->
